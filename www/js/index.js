94493048bb6a4ac63989d10782caab6c3a3c6ed1
Seth Steele <sethsteele@gmail.com>
2014-11-08 17:01:02 -0600

    yea ill keep committing

--- www/js/index.js
+++ www/js/index.js
@@ -1,23 +1,8 @@
 var wsServer = "ws://192.168.1.184:8080/";
 
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+var ws = WS(wsServer),
+    touching = null;
+
 var app = {
     // Application Constructor
     initialize: function() {
@@ -36,6 +21,17 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+
+        touching = navigator.accelerometer.watchAcceleration(
+            function(accel){
+                sendOrientation(accel);
+            }, 
+            function(){
+                console.log("PANIC");
+            }, {
+                frequency:250
+            });
+
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
@@ -50,30 +46,11 @@ var app = {
     }
 };
 
-var ws = WS(wsServer),
-    touching = null;
-
 $(document).ready(function(){
     ws.opened.then(function(){
         ws.send("setClientDeviceType",{
             deviceType: "guest"
         });
-    });
-
-    $("body").on("touchstart mousedown", function(){
-        touching = navigator.accelerometer.watchAcceleration(
-            function(accel){
-                sendOrientation(accel);
-            }, 
-            function(){
-                console.log("PANIC");
-            }, {
-                frequency:250
-            });
-    });
-
-    $("body").on("touchend mouseup", function(){
-        navigator.accelerometer.clearWatch(touching);
     });
 });
 
